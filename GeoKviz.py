e727e2dab1aa9c217c896f1c446503aa83f5ee7d
StefanNklc <59482378+StefanNklc@users.noreply.github.com>
2020-01-03 18:01:03 +0100

    Update GeoKviz.py

--- GeoKviz.py
+++ GeoKviz.py
@@ -1,14 +1,21 @@
+bod = 1
 print ("Dobar dan i dobrodošli u GeoKviz,pre početka bismo Vas zamolili da za odgovor unesete slovo a, b ili c. Hvala.")
-pitanje1 = input ("Koliko je visok Mont Everest? Ponuđeni odgovori su: a) 8231 ; b) 8848 ; c) 8523 ")
+pitanje1 = input ("Koliko je visok Mont Everest? Ponuđeni odgovori su:
+a) 8231
+b) 8848
+c) 8523 ") 
 pitanje1odg = "b"
 if pitanje1==pitanje1odg:
     print ("Odgovor je tačan.")
+    print (bod+3)
 else:
     print ("Odgovor nije tačan.")
+    print (bod-1) 
 pitanje2 = input ("Koja je najveća depresija na svetu? Ponuđeni odgovori su a) Mramorno more ; b) Crno more ; C) Mrtvo more ")
 pitanje2odg = "c"
 if pitanje2==pitanje2odg:
     print ("Odgovor je tačan.")
+    print (bod+3)
 else:
     print ("Odgovor nije tačan.")
     print (bod-1)
@@ -16,19 +23,25 @@ pitanje3 = input ("Po čemu je Afrika dobila ime? Ponuđeni odgovori su a) Po be
 pitanje3odg = "a"
 if pitanje3==pitanje3odg:
     print ("Odgovor je tačan.")
+    print (bod+3)
 else:
     print ("Odgovor nije tačan.")
+    print (bod-1)
 pitanje4 = input ("Koji moreuz spaja Aziju i Evropu? Ponuđeni odgovori su a) Bosfor ; b) Gibraltar ; c) Dardaneli ")
 pitanje4odg = "c"
 if pitanje4==pitanje4odg:
     print ("Odgovor je tačan.")
+    print (bod+3)
 else:
     print ("Odgovor nije tačan.")
+    print (bod-1)
 pitanje5 = input ("Koja boja rase je mongoloidna? Ponuđeni odgovori su a) bela ; b) žuta; c) crna ")
 pitanje5odg = "b"
 if pitanje5==pitanje5odg:
     print ("Odgovor je tačan.")
+    print (bod+3)
 else:
     print ("Odgovor nije tačan.")
+    print (bod-1)
 
 x = input ()
